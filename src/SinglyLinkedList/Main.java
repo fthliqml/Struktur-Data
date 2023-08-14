c2bf9a91c4d45e80cc01afc1581078975519ffdd
fthliqml <fatihuliqmalzzz@gmail.com>
2023-08-14 11:42:59 +0800

    Second

--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -7,7 +7,14 @@ public class Main {
         ll.add(1);
         ll.add(2);
         ll.add(3);
+        ll.add(4);
+        ll.add(5);
 
+        ll.swap(1,3);
+        ll.insert(0, 1);
+        ll.remove(6);
         ll.display();
+        System.out.println();
+        ll.get(3);
     }
 }
--- src/SinglyLinkedList/SLinkedList.java
+++ src/SinglyLinkedList/SLinkedList.java
@@ -24,14 +24,141 @@ public class SLinkedList {
         }
     }
 
+    public void insert(int value, int index){
+        int length = 0;
+        Node var = first;
+        while (var != null){
+            length++;
+            var = var.getNext();
+        }
+
+        if (index <= length){
+            Node node = new Node(value);
+            if (index == 1) {
+                node.setNext(first);
+                first = node;
+            } else if (index == length) {
+                add(value);
+            } else {
+                Node temp = first;
+                int i = 1;
+                while (temp != null){
+                    if(i == (index-1)){
+                        node.setNext(temp.getNext());
+                        temp.setNext(node);
+                        return;
+                    }
+                    i++;
+                    temp = temp.getNext();
+                }
+            }
+        }
+        else {
+            System.out.println("index out of bounds!");
+        }
+    }
+
+    public void remove(int index) {
+        int length = 0;
+        Node var = first;
+        while (var != null){
+            length++;
+            var = var.getNext();
+        }
+
+        if (index <= length){
+            if (index == 1){
+                first = first.getNext();
+            } else {
+                Node temp = first;
+                int i = 1;
+                while (temp != null){
+                    if (i == (index-1)){
+                        temp.setNext(temp.getNext().getNext());
+                    }
+                    i++;
+                    temp = temp.getNext();
+                }
+            }
+        } else {
+            System.out.println("Index out of bounds!");
+        }
+    }
+
+    public void swap(int index1, int index2){
+        if (index1 > index2) {
+            int var = index2;
+            index2 = index1;
+            index1 = var;
+        }
+
+        Node node1 = null;
+        Node node1Prev = null;
+        Node node2 = null;
+        Node node2Prev = null;
+        Node currentNode = first;
+        int i = 1;
+
+        while (currentNode != null){
+            if (i == index1){
+                node1 = currentNode;
+            } else if (i == (index1-1)) {
+                node1Prev = currentNode;
+            } else if (i == index2) {
+                node2 = currentNode;
+            } else if (i == (index2-1)) {
+                node2Prev = currentNode;
+            }
+            i++;
+            currentNode = currentNode.getNext();
+        }
+
+        Node node2Next = node2.getNext();
+        Node node1Next = node1.getNext();
+        node1.setNext(node2Next);
+
+       if (index2-index1 == 1) {
+           node2.setNext(node1);
+            if (index1 == 1){
+                first = node2;
+            }
+            else {
+                node1Prev.setNext(node2);
+            }
+       } else {
+           if (index1 == 1){
+               first = node2;
+           } else {
+               node1Prev.setNext(node2);
+           }
+           node2.setNext(node1Next);
+           node2Prev.setNext(node1);
+       }
+    }
+
+    public void get(int index){
+        Node temp = first;
+        int i = 1;
+        while (temp != null){
+            if (i == index){
+                System.out.println("get : " + temp.getValue());
+                return;
+            }
+            i++;
+            temp = temp.getNext();
+        }
+    }
 
     public void  display(){
         Node temp = first;
+        int size = 0;
         while (temp != null){
             System.out.print(temp.getValue() + " -> ");
             temp = temp.getNext();
+            size++;
         }
         System.out.println("NULL");
+        System.out.println("Ukuran Linked-list : " + size);
     }
 
 
