d79546c3ac8b991252265308c115e46442121a0b
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-09-11 07:38:08 +0800

    menambahkan dikit error

--- src/SelectionSortList/selectionSort.js
+++ src/SelectionSortList/selectionSort.js
@@ -1,15 +1,20 @@
 const RandomNumber = (count) =>{
+  if (count<=0) {
+    throw new Error('Parameter tidak boleh kurang dari sama dengan 0');
+  }else if (count > 10000) {
+    throw new Error('Parameter tidak melebihi 10000');
+  }
     const arrs = new Array();
     for (let i = 0; i < count ; i++) {
         let rand = Math.random();
-        const l = Math.floor(rand < 0.5 ? Math.random()* -100: Math.random() * 100);
+        const l = Math.floor(rand < 0.5 ? Math.random()* - 100000: Math.random() * 100000);
         arrs.push(l);
     }
     return arrs;
 }
 function SelectionSort (arr) {
   let i, j, getIndex, temp;
-
+  console.time("kecepatan ");
   for (i = 0; i < arr.length; i++) {
     temp = arr[i];
 
@@ -26,11 +31,12 @@ function SelectionSort (arr) {
     console.log("Selected Value : "+temp);
     console.log(arr);
   }
-  console.log('Hasil Akhir');
+  console.log('Hasil Akhir'); 
   console.log(arr);
+  console.timeEnd("kecepatan ");
 }
 
-const array = RandomNumber(10);
+const array = RandomNumber(100);
 console.log('Tampilan Awal');
 console.log(array);
 console.log("");
