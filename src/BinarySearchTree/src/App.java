a425f1d37eb2b17743e01c66d298e5c36d0a8eaa
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-10-07 17:12:57 +0800

    second commit

--- src/BinarySearchTree/src/App.java
+++ src/BinarySearchTree/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Tree tree = new Tree();
+
+        // Add elements to the tree
+        tree.add(50);
+        tree.add(30);
+        tree.add(70);
+        tree.add(20);
+        tree.add(40);
+        tree.add(60);
+        tree.add(80);
+
+        // Display the sorted nodes
+        System.out.println("Sorted Nodes:");
+        tree.display();
     }
 }
--- src/BinarySearchTree/src/Tree.java
+++ src/BinarySearchTree/src/Tree.java
@@ -1,3 +1,81 @@
 public class Tree {
+    private Node root;
+
+    public void root(Node root) {
+        this.root = root;
+    }
+
+    public boolean search(Node parent, int key) {
+        if (parent == null) {
+            return false;
+        } else {
+            if (parent.getKey() == key) {
+                return true;
+            }
+            if (parent.getKey() > key) {
+                return search(parent.getLeft(), key);
+            } else {
+                return search(parent.getRight(), key);
+            }
+        }
+    }
+
+    public boolean add(int key) {
+        Node node = new Node(key);
+
+        if (search(root, key)) {
+            return false;
+        }
+        if (root == null){
+            root = node;
+            return true;
+        }
+        insert(root, node);
+        return true ;
+    }
+
+    public boolean insert(Node parent, Node node){
+        if (parent.getKey() > node.getKey()) {
+            if (parent.getLeft() == null) {
+                parent.setLeft(node);
+            }else{
+                insert(parent.getLeft(), node);
+            }
+        }else{
+            if (parent.getRight() == null) {
+                parent.setRight(node);
+            }else{
+                insert(parent.getRight(), node);
+            }
+        }
+        return true;
+        
+    }
+    public boolean remove(int key){
+        if(!search(root, key)){
+            return false;
+        }
+        
+        return true;
+    }
+   
+    public void display(Node root, String prefix, boolean isLeft) {
+        if (root != null) {
+            System.out.println(prefix + (isLeft ? "├── " : "└── ") + root.getKey());
+            display(root.getLeft(), prefix + (isLeft ? "│   " : "    "), true);
+            display(root.getRight(), prefix + (isLeft ? "│   " : "    "), false);
+        }
+    }
     
+    public void display() {
+        display(root, "", true);
+    }
+    
+
+
+    public Node getRoot(){
+        return root;
+    }
 }
+
+
