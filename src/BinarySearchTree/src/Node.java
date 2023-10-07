5aaae8facb22381dd5dfc50891cd0c4e6483f162
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-10-07 14:03:22 +0800

    creating Node class

--- src/BinarySearchTree/src/Node.java
+++ src/BinarySearchTree/src/Node.java
@@ -1,3 +1,26 @@
 public class Node {
-    
+    private int key;
+    private Node right;
+    private  Node left;
+
+    public Node (int key){
+        this.key = key;
+    }
+
+    public void setRight(Node right){
+        this.right = right;
+    }
+    public void setLeft(Node left){
+        this.left = left;
+    }
+
+    public Node getRight(){
+        return right;
+    }
+    public Node getLeft(){
+        return left;
+    }
+    public int getKey(){
+        return key;
+    }
 }
